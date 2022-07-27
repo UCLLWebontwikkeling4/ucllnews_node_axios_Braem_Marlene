b2d922c26f9432183d19a244fe993d7323169f63
Marlène Braem <marlenebraem@hotmail.be>
2022-07-27 12:26:57 +0200

    add newsitem met url

--- app.ts
+++ app.ts
@@ -19,6 +19,15 @@ const server = http.createServer((req, res) => {
             // call data => build html with data => write html => end response
             makeGetRequest().then(data => buildHtml(data)).then(html => res.write(html)).then(() => res.end())
             break
+        case '/news/add':
+              //http://localhost:9000/news/add?title=test&text=hallo&author=marlene
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            let title = req.url.split('&')[0].split('=')[1]
+            let text = req.url.split('&')[1].split('=')[1]
+            let author = req.url.split('&')[2].split('=')[1]
+            makePostRequest(title, text, author).then(r => "error")
+            res.writeHead(302, { 'Location': '/news/all' })
+            res.end()
     }
 })
 
@@ -29,6 +38,11 @@ server.listen(port, hostname, () => {
 async function makeGetRequest () {
     let res = await axios.get('http://localhost:8080/Controller?command=Random')
     return res.data
+}
+
+async function makePostRequest (title, text, author) {
+    let res =  await axios.post('http://localhost:8080/Controller?command=Add' + "&title_input=" + title + "&text_input=" + text + "&author_input=" + author);
+    return res;
 }
 
 function buildHtml(data) {
